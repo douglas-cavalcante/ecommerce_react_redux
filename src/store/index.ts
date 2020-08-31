a63757b326611fe2a31e7f3fa5d81796d29c7080
douglas-cavalcante <henriquedouglas2803@gmail.com>
2020-08-31 16:29:51 -0300

    Adiciona DevTools

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,15 @@ import { createStore } from 'redux';
 import rootReducer from './modules/rootReducer';
 import { ICartState } from './modules/cart/types';
 
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 export interface IState {
   cart: ICartState;
 }
 
 const store = createStore(
-  rootReducer
+  rootReducer,
+  composeWithDevTools()
 );
 
 export default store;
